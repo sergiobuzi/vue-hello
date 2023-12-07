5d7b4f768b6751b07de9720ea1b184a78f2da8a4
SergioBuzi <sergio.buzi97@gmail.com>
2023-12-07 14:17:53 +0100

    script: adding message printed in DOM

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+//creo una variabile che mi salvi il createapp
+const { createApp } = Vue ;
+
+
+createApp({
+    data(){
+        return {
+            messaggio: 'Hello Vue!'
+        }
+    }
+}).mount('#app');
